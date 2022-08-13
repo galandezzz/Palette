4850cca738585ea7702982854a92f6f4fd24d87b
Vinicius Nakamura <hello@vnakamura.com>
2022-08-13 17:48:21 -0300

    Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Palette",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "Palette",
+            targets: ["Palette"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Palette",
+            dependencies: [],
+            path: "Source"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
